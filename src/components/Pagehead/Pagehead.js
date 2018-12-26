d1d5faea6a479e81a3def990b4b486ea8e926c26
andrzejchmura <chmura.and@gmail.com>
2018-12-26 16:03:28 +0100

    Update Pagehead component

--- src/components/Pagehead/Pagehead.js
+++ src/components/Pagehead/Pagehead.js
@@ -2,12 +2,12 @@ import React from "react";
 import css from "./pagehead.module.css";
 import { H2, H3 } from "../Typography/Typography";
 
-const PageHead = ({ children }) => {
+const PageHead = ({ title, subtitle }) => {
   return (
     <div className={css.container}>
       <div className={css.wrapper}>
-        <H2 className={css.title}>Labs</H2>
-        <H3 className={css.subtitle}>Parametric design experiments</H3>
+        <H2 className={css.title}>{title}</H2>
+        <H3 className={css.subtitle}>{subtitle}</H3>
       </div>
     </div>
   );
