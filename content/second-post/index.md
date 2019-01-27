bf81ac32c1b75cc90881c71c32042a97b752f53b
andrzejchmura <chmura.and@gmail.com>
2019-01-27 14:18:40 +0100

    Add source plugins

---
+++ content/second-post/index.md
@@ -0,0 +1,8 @@
+---
+title: Second one
+date: '2019-01-27'
+---
+
+And this is the second post.
+
+Lorem ipsum dolor sit amet.
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@ module.exports = {
     description: "Personal website",
     siteUrl: "https://chmura.io"
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: __dirname + "/content/"
+      }
+    },
+    "gatsby-transformer-remark"
+  ]
 };
