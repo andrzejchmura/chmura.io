6057a879f307e4a4b8674f7e8ad8890f1eb41c30
andrzejchmura <chmura.and@gmail.com>
2019-01-27 13:04:17 +0100

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: "Chmura.io"
+    title: "Chmura.io",
+    author: "Andrzej Chmura",
+    description: "Personal website",
+    siteUrl: "https://chmura.io"
   },
   plugins: ["gatsby-plugin-postcss"]
 };
