1c11f1da5a757129adb107e13658bfabf4e1e74c
andrzejchmura <chmura.and@gmail.com>
2019-09-10 10:56:10 +0200

    Update Hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 import { H1, H2, Link } from "../Typography/Typography";
 import css from "./hero.module.css";
 
-const Hero = props => {
+const Hero = () => {
   return (
     <div className={css.container}>
       <div className={css.wrapper}>
@@ -12,6 +12,9 @@ const Hero = props => {
             Currently working on web based mobile
             <strong> augmented reality </strong>experiences.
           </H2>
+          <a href="https://github.com/andrzejchmura" className={css.cta}>
+            GitHub
+          </a>
           <a href="mailto:andrzej@chmura.io" className={css.cta}>
             Get in touch ->
           </a>
